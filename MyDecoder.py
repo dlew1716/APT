bd7e381cdab2a25c74a7f157179f37549063bcf0
Dylan Lewis <dbl1716@gmail.com>
2016-02-03 09:50:11 -0500

    Python script returns image but does not interpolate or save

--- MyDecoder.py
+++ MyDecoder.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
-import matcompat
+import time
 import wave
 import struct
 from pylab import *
 import scipy.signal as signal
+import scipy.interpolate as interpolate
 
 #pleb
 
@@ -31,6 +32,7 @@ WaveFile = wavLoad(location)
 sigarr = wavLoad(location)[0]
 sampRate = double(wavLoad(location)[1])
 sigarr = np.array(sigarr)
+
 # WavSig = wave.open(location, 'r')
 # #[x1, fs] = audioread(location)
 # # x = x1[0:length(x1),0]
@@ -52,27 +54,35 @@ sigarr = np.array(sigarr)
 search = 7.
 cutoff_hz = double(1000.0)
 
-print type(cutoff_hz)
-print type(sampRate)
+# print type(cutoff_hz)
+# print type(sampRate)
 
-max_amplitude = np.max(np.absolute(sigarr))
+max_amplitude = np.amax(np.absolute(sigarr))
+
+
+
 #% find the maximum amplitude
-sigarr = np.divide(sigarr,max_amplitude)
+sigarr = np.true_divide(sigarr,max_amplitude)
+
 #% normalize the input signal
 sigarr = np.subtract(sigarr,np.mean(sigarr))
+
 
 #Design Filter
 fir_coeff = signal.firwin(3, cutoff_hz/(sampRate/2))
 
 #rectify
-ensig = np.absolute(sigarr)
+sigarr = np.absolute(sigarr)
+
 
 #Low pass filter
 sigarr = scipy.signal.lfilter(fir_coeff,1, sigarr)
 
 sigarr = np.subtract(sigarr,np.mean(sigarr))
 
-sigarr = np.divide(sigarr,np.max(sigarr))
+sigarr = np.true_divide(sigarr,np.max(sigarr))
+
+y = sigarr
 
 
                 
@@ -86,51 +96,79 @@ cA = signal.square(1040*t*(2*pi),0.5)
 hA = np.convolve(cA,sigarr)
 
 #%%Preforms the correlation
-syncA = hA[int(length(cA))-1:length(hA)]
+syncA = hA[int(np.size(cA))-1:np.size(hA)]
 
 
-while True:
-    x=1
 #%%Removes the convolution tails
-[H, I] = matcompat.max(syncA)
-sampsbefore = np.floor(matdiv(I, fs/2.))-5.
-sampsafter = np.floor(matdiv(length(syncA)-I, fs/2.))
-#%plot(syncA)
-#% axis([3.095e6 3.14e6 14 19.5])
-synclocs = np.ones((sampsbefore+sampsafter), 1.)
-currIndex = np.round((I-fs/2.))
+
+H = np.amax(syncA)
+I = np.argmax(syncA)
+
+sampsbefore = np.floor(np.true_divide(I, sampRate/2.))-5.
+sampsafter = np.floor(np.true_divide(np.size(syncA)-I, sampRate/2.))
+
+synclocs = np.ones((sampsbefore+sampsafter))
+currIndex = np.round((I-sampRate/2.))
+
+
 for i in np.arange(1., (sampsbefore)+1):
-    [locH, locI] = matcompat.max(syncA[int(currIndex-matdiv(fs, search))-1:currIndex+matdiv(fs, search)])
-    globI = currIndex-matdiv(fs, search)+locI-1.
+    locH = np.amax(syncA[int(currIndex-sampRate/search)-1:int(currIndex+sampRate/search)])
+    locI = np.argmax(syncA[int(currIndex-sampRate/search)-1:int(currIndex+sampRate/search)])
+    globI = currIndex-sampRate/search+locI-1.
     synclocs[int(i)-1] = globI
-    currIndex = np.round((globI-fs/2.))
+    currIndex = np.round((globI-sampRate/2.))
     #% hold on
     #% plot(synclocs,syncA(round(synclocs)),'r*')
-    
-synclocs = np.array(np.vstack((np.hstack((I)), np.hstack((synclocs)))))
+
+
+synclocs = np.insert(synclocs,0,I)
+
 synclocs[0:sampsbefore+1.] = np.flipud(synclocs[0:sampsbefore+1.])
-currIndex = np.round((I+fs/2.))
+currIndex = np.round((I+sampRate/2.))
+
 for i in np.arange(1., (sampsafter-2.)+1):
-    [locH, locI] = matcompat.max(syncA[int(currIndex-matdiv(fs, search))-1:currIndex+matdiv(fs, search)])
-    globI = currIndex-matdiv(fs, search)+locI-1.
+    locH = np.amax(syncA[int(currIndex-sampRate/search)-1:int(currIndex+sampRate/search)])
+    locI =  np.argmax(syncA[int(currIndex-sampRate/search)-1:int(currIndex+sampRate/search)])
+    globI = currIndex-sampRate/search+locI-1.
     synclocs[int((i+sampsbefore+1.))-1] = globI
-    currIndex = np.round((globI+fs/2.))
+    currIndex = np.round((globI+sampRate/2.))
+
+
+
+
+
+
+#%plot(syncA)
+#% axis([3.095e6 3.14e6 14 19.5])
+
+
+    
+
     
 #%     hold on
 #%     plot(synclocs,syncA(round(synclocs)),'r*')
-for j in np.arange(1., (length(synclocs))+1):
+for j in np.arange(1., (np.size(synclocs))+1):
     if j == 1.:
-        imagemat = y[int(synclocs[int(j)-1])-1:synclocs[int(j)-1]+np.round((fs/2.))].conj().T
+        imagemat = y[int(synclocs[int(j)-1])-1:synclocs[int(j)-1]+np.round((sampRate/2.))]
     
-    
-    imagemat = np.array(np.vstack((np.hstack((imagemat)), np.hstack((y[int(synclocs[int(j)-1])-1:synclocs[int(j)-1]+np.round((fs/2.))].conj().T)))))
-    
+    newmat = y[int(synclocs[int(j)-1])-1:synclocs[int(j)-1]+np.round((sampRate/2.))]
+    imagemat = np.vstack((imagemat,newmat))
+
+
 imagemat = np.flipud(imagemat)
 imagemat = np.fliplr(imagemat)
-imagesc(imagemat)
-colormap(plt.gray)
-[rows, columns, numberOfColorChannels] = matcompat.size(imagemat)
-stretchedImage = imresize(imagemat, np.array(np.hstack((rows, 2080.))), 'lanczos3')
-stretchedImage = stretchedImage-matcompat.max(stretchedImage.flatten(1))
-stretchedImage = matdiv(stretchedImage, matcompat.max(stretchedImage.flatten(1)))
-imwrite(stretchedImage, 'myimage8.png', 'BitDepth', 16.)
+print 'done'
+
+plt.imshow(imagemat,cmap = cm.Greys_r)
+plt.show()
+
+# while True:
+#     time.sleep(.2)
+
+# imagesc(imagemat)
+# colormap(plt.gray)
+# [rows, columns, numberOfColorChannels] = matcompat.size(imagemat)
+# stretchedImage = imresize(imagemat, np.array(np.hstack((rows, 2080.))), 'lanczos3')
+# stretchedImage = stretchedImage-matcompat.max(stretchedImage.flatten(1))
+# stretchedImage = matdiv(stretchedImage, matcompat.max(stretchedImage.flatten(1)))
+# imwrite(stretchedImage, 'myimage8.png', 'BitDepth', 16.)
