61956d3e1735dcf59bf4a28fa7332c1c4309710b
Dylan Lewis <dylanlewis@Dylans-MacBook-Pro.local>
2016-10-22 13:43:13 -0400

    0-1

--- MyDecoder.cc
+++ MyDecoder.cc
@@ -440,7 +440,7 @@ int main(int argc, char *argv[] ) {
 		for(int j = 0;j < floor(fs/2);j++){
 
 			//printf("%f\n",obuf[i*int(floor(fs/2))+j]);
-			png.plotHSV(i*int(floor(fs/2)),j, 0.0,0.0, obuf[i*int(floor(fs/2))+j]);
+			png.plotHSV(i*int(floor(fs/2))+1,j+1, 0.0,0.0, obuf[i*int(floor(fs/2))+j]);
 			//png.plotHSV(i*int(floor(fs/2)),j, 0.5, 0.5,0.5);
 
 		}
